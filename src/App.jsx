4c8e8897211037290ee6c0cee2a7190ab8c6fad2
mohitUpraity <mohitupraity123@gmail.com>
2025-08-21 22:10:16 +0530

    main content showing logic done now creating main content

--- src/App.jsx
+++ src/App.jsx
@@ -3,19 +3,18 @@ import gsap from "gsap";
 import { useGSAP } from "@gsap/react";
 
 const App = () => {
-  const [isMain, setIsMain] = useState(false);
+  const [showMain, setShowMain] = useState(false);
   //on main change
   useEffect(() => {
-    console.log(isMain);
-  }, [isMain]);
+    console.log(showMain);
+  }, [showMain]);
   //animation of text
   useGSAP(() => {
     const tl = gsap.timeline({
-        onComplete:()=>{
-            setIsMain(true)
-        }
-    }
-    );
+      onComplete: () => {
+        setShowMain(true);
+      },
+    });
     tl.to(".vi-mask-group", {
       scale: 30,
       duration: 2,
@@ -33,50 +32,51 @@ const App = () => {
     tl.to(".vi-mask-group", { opacity: 0, duration: 0.5, delay: -1 });
   });
   return (
-    <div className="svg fixed top-0 left-0 z-[100] w-full h-screen overflow-hidden bg-black ">
-      <svg
-        viewBox="0 0 800 600"
-        preserveAspectRatio="xMidYMid slice"
-        className=""
-      >
-        <defs className="">
-          <mask id="viMask">
-            <rect width="100%" height="100%" fill="black" />
-            <g
-              className="vi-mask-group  "
-              style={{ transformBox: "view-box", transformOrigin: "50% 50%  " }}
-            >
-              <text
-                x="50%"
-                y="50%"
-                textAnchor="middle"
-                fill="white"
-                dominantBaseline="middle"
-                fontFamily="black"
-                fontSize="250"
-                className="text-center"
-              >
-                VI
-              </text>
-            </g>
-          </mask>
-        </defs>
-        {/* <rect width="100%" height="100%" fill="white" mask="url(#viMask)" /> */}
-        <image
-          href="./bg.png"
-          width="100%"
-          height="100%"
+    <div className="svg fixed top-0 left-0 z-[100] w-full h-screen overflow-hidden text-white bg-black ">
+      {!showMain && (
+        <svg
+          viewBox="0 0 800 600"
           preserveAspectRatio="xMidYMid slice"
-          mask="url(#viMask)"
-        />
-      </svg>
-      {isMain && (
-        // <div className="main-content text-white text-4xl z-2">
-        //   <h1>Main Content</h1>
-        // </div>
-        console.log("main content")
+          className=""
+        >
+          <defs className="">
+            <mask id="viMask">
+              <rect width="100%" height="100%" fill="black" />
+              <g
+                className="vi-mask-group  "
+                style={{
+                  transformBox: "view-box",
+                  transformOrigin: "50% 50%  ",
+                }}
+              >
+                <text
+                  x="50%"
+                  y="50%"
+                  textAnchor="middle"
+                  fill="white"
+                  dominantBaseline="middle"
+                  fontFamily="black"
+                  fontSize="250"
+                  className="text-center"
+                >
+                  VI
+                </text>
+              </g>
+            </mask>
+          </defs>
+          {/* <rect width="100%" height="100%" fill="white" mask="url(#viMask)" /> */}
+          <image
+            href="./bg.png"
+            width="100%"
+            height="100%"
+            preserveAspectRatio="xMidYMid slice"
+            mask="url(#viMask)"
+          />
+        </svg>
       )}
-
+      <div>
+        <h1>Main content</h1>
+      </div>
     </div>
   );
 };
