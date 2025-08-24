11f9ef7ef9ddb8fc6fd1576fc1624b938705087e
mohitUpraity <mohitupraity123@gmail.com>
2025-08-24 20:18:19 +0530

    all the animations done just left nav bar and footer if needed animations fixed for both lg and md

--- src/App.jsx
+++ src/App.jsx
@@ -15,6 +15,7 @@ const App = () => {
       gsap.to(".buildings-layer", { x: xMove * 0.4 });
       gsap.to(".sky-layer", { x: xMove * 0.3 });
       gsap.to(".main-text", { x: xMove * -1.5 });
+      gsap.fromTo(".down-arrow", { y: -5.5 }, { y: 0, repeat: -1, yoyo: true });
     });
   }, [showMain]);
 
@@ -74,13 +75,21 @@ const App = () => {
       delay: -2,
       ease: "power2.inOut",
     });
-    tl.from(".main-text", {
+    tl.from(".main-text ", {
       // rotate: -30,
-      y: -40,
+      y: -30,
       // scale: 10,
-      duration: 2,
+      duration: 1,
       delay: 0,
       ease: "power4.Out",
+    });
+    tl.from(".VI ", {
+      // rotate: -30,
+      y: -400,
+      // scale: 10,
+      duration: 0.5,
+      delay: -0.2,
+      ease: "back.out",
     });
   });
 
@@ -157,21 +166,39 @@ const App = () => {
             className=" buildings-layer max-w-none w-full h-full object-cover"
           />
         </div>
+
         {/* title layer */}
         <div
           className="absolute inset-0 flex flex-col items-center justify-between font-sans "
           style={{ fontFamily: "black" }}
         >
-          <header>
-            <h1 className="text-xl text-center">navbar</h1>
+          <header className="w-full  flex p-3   justify-between  ">
+            <h1 className="text-xl text-center underline-0">R*</h1>
+            {/* <div className="flex gap-5">
+              <h1 className="text-xl text-center">navbar</h1>
+              <h1 className="text-xl text-center">navbar</h1>
+              <h1 className="text-xl text-center">navbar</h1>
+            </div> */}
+
+            {/* <h1 className="text-xl text-center">navbar</h1> */}
           </header>
-          <main className=" main-text font-bold text-center flex flex-col gap-3 text-8xl lg:text-[12rem] ">
-            <h1 className="-translate-x-24 underline">grand</h1>
-            <h1 className=" underline">theft</h1>
+          <main className=" main-text font-bold text-center flex flex-col gap-3 text-8xl lg:text-[11rem] ">
+            <h1 className="-translate-x-24 underline ">grand</h1>
+            <div className="  flex gap-3 lg:gap-5   ">
+              <h1 className="underline">theft</h1>
+              {/* <h1 className="text-9xl  ">VI</h1> */}
+
+              <h1 className="text-8xl VI bg-gradient-to-r from-orange-500 to-blue-500 bg-clip-text text-transparent lg:text-[11rem]  ">
+                VI
+              </h1>
+            </div>
             <h1 className="-translate-x-22 underline">auto</h1>
           </main>
-          <footer>
-            <p className="text-xl text-center">Footer Content</p>
+
+          <footer className="flex w-full px-5 justify-between items-center">
+            <p className="text-xl text-center">Play now</p>
+            <p className="text-xl text-center">steam</p>
+            <p className="text-xl text-center">download</p>
           </footer>
         </div>
 
@@ -182,6 +209,13 @@ const App = () => {
             alt="character"
             className="character-layer w-[30vw] h-auto object-contain "
           />
+        </div>
+        {/* circle */}
+        <div className="w-full absolute bottom-0 p-5">
+          <div className="absolute bottom-0 left-1/2 -translate-x-1/2 -rotate-90 text-3xl mix-blend-overlay down-arrow flex ">
+            <p className="rotate-90 text-sm">scroll</p>
+            <p>{"<"}</p>
+          </div>
         </div>
       </div>
     </div>
